56a4bd5c87d1ae14048b803b8c1d12558315990d
Thomas Beioley <31884223+mushola@users.noreply.github.com>
2025-07-18 18:52:42 +0100

    Add game script to js file
    Completed script for game, which includes 5 rounds vs the computer.

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log("Hello World")
+
+const moves = ['rock', 'paper', 'scissors'];
+const outcome = [[1,0,2],
+                 [2,1,0],
+                 [0,2,1]] // [human][computer], 0-lose, 1-draw, 2-win
+
+
+function getComputerChoice() {
+  return Math.floor(Math.random() * 3);
+}
+
+function getHumanChoice() {
+  let h = prompt('Enter your move (rock, paper or scissors)').toLowerCase();
+  return moves.indexOf(h);
+}
+
+
+function playGame () {
+
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    
+    switch (outcome[humanChoice][computerChoice]) {
+      case 0:
+        console.log("You lose! " + moves[humanChoice] + " gets beaten by " + moves[computerChoice] + ".");
+        computerScore++; 
+        break;
+      case 1:
+        console.log("Draw! Both played " + moves[humanChoice]);
+        break;
+      case 2:
+        console.log("You win! " + moves[humanChoice] + " beats " + moves[computerChoice] + ".");
+        humanScore++;
+        break;
+    }
+    console.log("human: " + humanScore + ", computer: " + computerScore);
+  }
+
+  for (i=0; i<5; i++) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice)
+  }
+
+}
+
+playGame()
